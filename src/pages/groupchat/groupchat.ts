31ebdb94a79d9401d3a150fbe452a60e85cac81e
Calvin <cmogodi@gmail.com>
2019-01-03 11:10:08 +0200

    chat

--- src/pages/groupchat/groupchat.ts
+++ src/pages/groupchat/groupchat.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { GlobalVariablesProvider } from '../../providers/global-variables/global-variables';
 import { UserProvider } from '../../providers/user/user';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs';
+import { NgZone } from '@angular/core';
 
 /**
  * Generated class for the GroupchatPage page.
@@ -12,16 +13,22 @@ import { Observable } from 'rxjs';
  * Ionic pages and navigation.
  */
 
+
 @Component({
   selector: 'page-groupchat',
   templateUrl: 'groupchat.html',
 })
+
 export class GroupchatPage {
   user: any;
   displayName = "";
   itemValue = '';
   chats: any[];
-  constructor(public db: AngularFireDatabase, public navCtrl: NavController, public navParams: NavParams, public globalVariables: GlobalVariablesProvider, public userProvider: UserProvider) {
+  message = '';
+  rows = 1;
+  @ViewChild('myInput', { read: ElementRef }) myInput: ElementRef;
+
+  constructor(private zone: NgZone, public db: AngularFireDatabase, public navCtrl: NavController, public navParams: NavParams, public globalVariables: GlobalVariablesProvider, public userProvider: UserProvider) {
 
     db.database.ref().child('chats').orderByChild('date').on('value', (snapshot) => {
       var orders = snapshot.val();
@@ -42,6 +49,20 @@ export class GroupchatPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GroupchatPage');
+  }
+
+  resize() {
+    this.myInput.nativeElement.style.height = this.myInput.nativeElement.scrollHeight + 'px';
+  }
+
+  sendMessage(str){
+    let message = {
+      message: str,
+      date: new Date().toString(),
+      user: this.displayName,
+    };
+    this.db.list('/chats').push(message);
+    this.message = '';
   }
 
 }
